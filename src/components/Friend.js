1d7a752f66fce8065e829587d3af26dfb5211c88
Sadia Gray <sadiagray17@gmail.com>
2023-06-22 14:51:32 -0500

    finished

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-export default function Friend({ info, action }) {
+
+const kf = keyframes`
+50% {
+  transform: scale(0.8);
+}
+100% {
+  transform: scale(1);
+  opacity: 1 ;
+}
+`
+
+const StyledFriend = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    padding: 8px;
+    border-button: 2px solid white;
+
+    background-color: ${pr => pr.theme.primaryColor};
+    color: ${ pr => pr.theme.white};
+
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      transition: all 0.2s ease-in-out;
+      background-color: ${pr => pr.theme.secondaryColor};
+    }
+
+  &::before {
+    content: "${pr => pr.bestie ? "💕" : "❤️" }";
+  }
+
+  button {
+    background-color: ${pr => pr.theme.tertiaryColor};
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+
+  transform scale(2); 
+  opacity: 0;
+  animation: ${kf} 0.3s ease-in-out forwards;
+`
+
+export default function Friend({ info, action, bestie }) {
   return (
-    <div className='friend'>
+    <StyledFriend bestie={bestie}>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
-  )
+    </StyledFriend>
+   )
 }
